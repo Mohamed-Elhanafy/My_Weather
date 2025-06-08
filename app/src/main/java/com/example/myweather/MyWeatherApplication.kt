b0c6bcc9ffc650373b43707c3bcadd1904d294d4
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-06-08 14:08:05 +0300

    remove debug logging from Koin initialization in MyWeatherApplication

--- app/src/main/java/com/example/myweather/MyWeatherApplication.kt
+++ app/src/main/java/com/example/myweather/MyWeatherApplication.kt
@@ -3,16 +3,13 @@ package com.example.myweather
 import android.app.Application
 import com.example.myweather.di.appModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
 
 class MyWeatherApplication : Application(){
     override fun onCreate() {
         super.onCreate()
 
         startKoin{
-            androidLogger(Level.DEBUG)
             androidContext(this@MyWeatherApplication)
             modules(appModule)
         }
