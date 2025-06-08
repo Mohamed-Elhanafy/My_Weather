0e83246a421ee9ab322650ef1013dcdd82278453
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-06-08 12:02:42 +0300

    add application class and configure Ktor client logging

--- app/src/main/java/com/example/myweather/di/appModule.kt
+++ app/src/main/java/com/example/myweather/di/appModule.kt
@@ -1,11 +1,15 @@
 package com.example.myweather.di
 
+import com.example.myweather.data.repository.WeatherRepositoryImpl
+import com.example.myweather.domain.repository.WeatherRepository
+import com.example.myweather.domain.usecase.GetWeatherUseCase
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule = module {
@@ -19,9 +23,17 @@ val appModule = module {
             }
 
             install(Logging) {
-                level = LogLevel.ALL
+                level = LogLevel.INFO
+                logger = object : io.ktor.client.plugins.logging.Logger {
+                    override fun log(message: String) {
+                        println("HTTP Client: $message")
+                    }
+                }
             }
-
         }
     }
+
+    single<WeatherRepository> { WeatherRepositoryImpl(get()) }
+    singleOf(::GetWeatherUseCase)
+
 }
